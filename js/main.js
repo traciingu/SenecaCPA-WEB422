cc9830602c2632705520b9f06adf86424c9249dc
traciingu <traciinguyen@hotmail.com>
2019-01-16 17:49:17 -0500

    msg

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/*********************************************************************************
+*  WEB422 – Assignment 1
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: Tracy Nguyen Student ID: 127270171 Date: JAN 2019
+*
+*  Link: https://web422-assignment1-tn.herokuapp.com/
+*  https://git.heroku.com/web422-assignment1-tn.git
+********************************************************************************/ 
