4c5b0464b7c5ec0bacbe9235a811b17b1fb4c916
traciingu <traciinguyen@hotmail.com>
2019-01-18 23:27:08 -0500

    commit

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@
 ********************************************************************************/
 
 
-$("document").ready(function () {
+$(document).ready(function () {
     console.log("jQuery working!");
 
 
